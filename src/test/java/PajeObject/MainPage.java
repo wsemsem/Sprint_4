4337e1721ac53e2541e437f234b1946acbf601ec
Alexandr Parkhamovich <sanservera@gmail.com>
2024-12-02 19:57:30 +0300

    First commit

---
+++ src/test/java/PajeObject/MainPage.java
@@ -0,0 +1,45 @@
+package PajeObject;
+
+import org.openqa.selenium.*;
+
+
+
+//Класс главной страницы
+public class MainPage {
+
+    private WebDriver driver;
+
+    //Кнопка "Заказать" в Хедере
+    private By orderButtonHeader = By.className("Button_Button__ra12g");
+
+    //Кнопка "Заказать" в середине страницы
+    private By orderButtonMiddle = By.xpath(".//div[@class='Home_FinishButton__1_cWm']//button");
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Метод клика по кнопке в хедере
+    public void clickOrderButtonHeader() {
+        driver.findElement(orderButtonHeader).click();
+    }
+
+    //Метод клика по кнопке в середине страницы
+    public void clickOrderButtonMiddle() {
+        driver.findElement(orderButtonMiddle).click();
+    }
+
+    //Скролл до необходимой кнопки
+    public void scrollToOrderButtonMiddle() {
+        WebElement element = driver.findElement(orderButtonMiddle);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/test/java/PajeObject/RegistrationOrderStep1.java
@@ -0,0 +1,65 @@
+package PajeObject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+//Класс 1-го шага формы для заказа самоката
+public class RegistrationOrderStep1 {
+
+    private WebDriver driver;
+
+    //Поле для ввода "Имя"
+    private By nameField = By.xpath(".//input[@placeholder='* Имя']");
+
+    //Поле для ввода "Фамилия"
+    private By surenameField = By.xpath(".//input[@placeholder='* Фамилия']");
+
+    //Поле для ввода "Адрес"
+    private By addressField = By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']");
+
+    //Поле для ввода "Станция метро"
+    private By subwayField = By.xpath(".//input[@placeholder='* Станция метро']");
+
+    //Поле для ввода "Телефон"
+    private By phoneNumberField = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
+
+    //Кнопка "Далее"
+    private By nextStepButton = By.xpath(".//button[text()='Далее']");
+
+    public RegistrationOrderStep1(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    //Метод заполнения поля "Имя"
+    public void setNameField(String firstName) {
+        driver.findElement(nameField).sendKeys(firstName);
+    }
+
+    //Метод заполнения поля "Фамилия"
+    public void setSurenameField(String lastName) {
+        driver.findElement(surenameField).sendKeys(lastName);
+    }
+
+    //Метод заполнения поля "Адрес"
+    public void setAddressField(String address) {
+        driver.findElement(addressField).sendKeys(address);
+    }
+
+    //Метод заполнения поля "Станция метро"
+    public void setSubwayField(String subwayAddress) {
+        driver.findElement(subwayField).click();
+        WebElement dropDown = driver.findElement(By.xpath(".//div[contains(text(), '" + subwayAddress + "')]"));
+        dropDown.click();
+    }
+
+    //Метод для заполнения поля "Телефон"
+    public void setPhoneNumberField(String phoneNumber) {
+        driver.findElement(phoneNumberField).sendKeys(phoneNumber);
+    }
+
+    //Метод нажатия на кнопку "Далее"
+    public void clickNextStepButton() {
+        driver.findElement(nextStepButton).click();
+    }
+}
